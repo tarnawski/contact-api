afc8a80415cee411ff06bbb861b9f386da28eb7d
Tomasz Tarnawski <ttarnawski@pgs-soft.com>
2017-06-22 11:28:30 +0200

    Add vagrant

--- index.php
+++ index.php
@@ -15,17 +15,17 @@ $app = new ContactApi($parameters);
 
 try {
     $data = json_decode(file_get_contents('php://input'), true);
-    $result = $app->send($data['from'], $data['body']);
-    if($result) {
+    $message = sprintf('%s -  %s', $data['body'], $data['from']);
+    $result = $app->send($message);
+
+    if ($result) {
         header('Content-Type: application/json');
         echo json_encode(['status' => 'Success']);
     } else {
         header('Content-Type: application/json');
         echo json_encode(['status' => 'Error']);
     }
-
 } catch (Exception $e) {
     header('Content-Type: application/json');
     echo json_encode(['status' => 'Error']);
 }
-
--- src/ContactApi.php
+++ src/ContactApi.php
@@ -5,7 +5,11 @@ class ContactApi
     /** @var string */
     private $host;
 
+    /** @var string */
     private $port;
+
+    /** @var string */
+    private $address;
 
     /** @var string */
     private $username;
@@ -26,22 +30,23 @@ class ContactApi
     public function __construct($parameters) {
         $this->host = $parameters['host'];
         $this->port = $parameters['port'];
+        $this->address = $parameters['address'];
         $this->username = $parameters['username'];
         $this->password = $parameters['password'];
         $this->subject = $parameters['subject'];
         $this->target = $parameters['target'];
     }
 
-    public function send($from = null, $body = null)
+    public function send($body = null)
     {
         $transport = (new Swift_SmtpTransport($this->host, $this->port));
-        $transport ->setUsername($this->username);
-        $transport ->setPassword($this->password);
+        $transport->setUsername($this->username);
+        $transport->setPassword($this->password);
+
 
         $mailer = new Swift_Mailer($transport);
-
         $message = new Swift_Message($this->subject);
-        $message->setFrom($from);
+        $message->setFrom($this->address);
         $message->setTo($this->target);
         $message->setBody($body);
 
